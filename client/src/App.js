722f74745fa33a5fc1d774805b888f90a303071f
Ricardo Cláudio <0102525@softinsa.corp>
2022-03-17 16:12:28 +0000

    resting deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            multi docker application
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
